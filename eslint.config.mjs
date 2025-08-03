1bfd8f3a0b30e042340832061c65cfb07a7dcb50
tom96da <129831775+tom96da@users.noreply.github.com>
2025-08-04 02:17:56 +0900

    Update ESLint config.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,12 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
+import node from 'eslint-plugin-n'
+import promise from 'eslint-plugin-promise'
+import stylistic from '@stylistic/eslint-plugin'
 
-export default withNuxt(
+export default withNuxt([
   // Your custom configs here
-)
+  node.configs['flat/recommended-module'],
+  promise.configs['flat/recommended'],
+  stylistic.configs['recommended'],
+])
