159da5d2a77b55306f8546f6ea953b6a5ffac087
tom96da <129831775+tom96da@users.noreply.github.com>
2025-08-17 21:38:01 +0900

    Move code to `src/`.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,8 +11,14 @@ export default defineNuxtConfig({
     '@vee-validate/nuxt',
   ],
   devtools: { enabled: true },
+  dir: {
+    shared: 'src/shared/',
+    public: 'src/public/',
+  },
+  srcDir: 'src/app/',
+  serverDir: 'src/server/',
   alias: {
-    '#server': fileURLToPath(new URL('server', import.meta.url)),
+    '#server': fileURLToPath(new URL('src/server', import.meta.url)),
   },
   compatibilityDate: '2025-07-15',
   eslint: {
